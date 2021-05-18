e1f99403f89dd030b30e9a694829503b04537e38
unknown <sungkd123@outlook.com>
2021-05-19 00:17:41 +0530

    if else statement files

---
+++ ifelse.js
@@ -0,0 +1,19 @@
+var learningJavaScript = true;
+var age = 15;
+
+if (learningJavaScript) {
+    document.write('Person is learning');
+}
+else{
+    document.write('Not learning');
+}
+
+if(learningJavaScript == false) {
+    document.write('\nPerson is learning');
+}
+else if(age > 25) {
+    document.write('\nStill learning');
+}
+else {
+    document.write('\nNot learning');
+}
