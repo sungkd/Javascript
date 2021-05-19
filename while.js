2e2b1ece254cce828ec5534013d6a92384505d58
unknown <sungkd123@outlook.com>
2021-05-19 11:22:42 +0530

    Added while loop files

---
+++ while.js
@@ -0,0 +1,18 @@
+/* While Loop Syntax
+    while (condition) 
+    {
+        statement1
+        statement2
+        ....
+    }
+
+*/
+
+var age = 45;
+
+while(age >= 45) 
+{
+    console.log('Eligible for vaccination');
+    age--;
+}
+document.write('Vaccination is halted for this group');
