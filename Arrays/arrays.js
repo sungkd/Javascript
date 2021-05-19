44e2488150f549562b6be3919fe8daa7c47d1d29
unknown <sungkd123@outlook.com>
2021-05-19 18:31:59 +0530

    Added Array files

---
+++ Arrays/arrays.js
@@ -0,0 +1,15 @@
+/* Syntax 
+ var <name> = [];
+*/
+
+var myArray = []; //Define empty array
+var myArray1 = new Array(5);  //Define array with specific length
+
+myArray[0] = 100; //Stored 100 at 0th index of array
+myArray[1] = 400;
+
+var newArray = ['sunny', 'gaikwad', 14];
+
+document.write(myArray);
+document.write('<br>');
+document.write(newArray);
